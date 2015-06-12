7aa243273a6e124b137169bad81d1f29e664749c
Alexander Reitzel <funtimecoding@gmail.com>
2015-06-12 11:41:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Java Mysql Example
+
+Compile the program.
+
+```sh
+mvn clean install
+```
+
+Run the program.
+
+```sh
+java -jar target/java-mysql-example-1.0-SNAPSHOT.jar
+```
---
+++ src/main/java/org/funtimecoding/java/mysql/example/ExampleMain.java
@@ -0,0 +1,15 @@
+package org.funtimecoding.java.mysql.example;
+
+public class ExampleMain {
+
+    public static void main(String[] args)
+    {
+        ExampleMain main = new ExampleMain();
+        main.run();
+    }
+
+    public void run()
+    {
+        System.out.println("hi");
+    }
+}
